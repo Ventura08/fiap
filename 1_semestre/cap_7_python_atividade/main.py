dbad4d8a29c516bd817de8a3ef390f8a115438a6
Ventura08 <estevaotevu08@gmail.com>
2024-03-06 21:32:04 -0300

    .

--- 1_semestre/cap_7_python_atividade/main.py
+++ 1_semestre/cap_7_python_atividade/main.py
@@ -1,9 +1,9 @@
-anos_fumando = float(input("A quantos anos você fuma? "))
-valor_maco = float(input("Qual o valor atual do maço de cigarros? R$ "))
-macos_por_dia = float(input("Quantos maços você fuma por dia, em média? "))
+anos_fumando: float = float(input("A quantos anos você fuma? "))
+valor_maco: float = float(input("Qual o valor atual do maço de cigarros? R$ "))
+macos_por_dia: float = float(input("Quantos maços você fuma por dia, em média? "))
 
-dias_por_ano = 365.25
-montante_gasto = anos_fumando * dias_por_ano * macos_por_dia * valor_maco
+dias_por_ano: float = 365.25
+montante_gasto: float = anos_fumando * dias_por_ano * macos_por_dia * valor_maco
 
 if montante_gasto < 20000:
     mensagem = f"Com o valor R$ {montante_gasto:.2f}, você poderia ter dado entrada em um carro."
